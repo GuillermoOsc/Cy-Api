6c574ec87a8dac5d52143f41b88d4752e96a9846
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-18 01:01:20 -0300

    first commit

---
+++ cypress/e2e/predictor-de-edad.cy.js
@@ -0,0 +1,10 @@
+describe('Predictor de edad', () => {
+    const originalUrl = 'https://api.agify.io?name=meelad'
+    const endpoint = 'https://api.agify.io'
+    
+    it('Verificar los datos devueltos', () => {
+        const parametro = 'name';
+        const name = 'Pedro'
+
+    });
+});
