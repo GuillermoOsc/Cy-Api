cfcf08d2ec11ed3df34e8b9ec04144268ffa38da
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-19 02:50:12 -0300

    Verificación de los datos de la respuesta

--- cypress/e2e/predictor-de-edad.cy.js
+++ cypress/e2e/predictor-de-edad.cy.js
@@ -4,7 +4,14 @@ describe('Predictor de edad', () => {
     
     it('Verificar los datos devueltos', () => {
         const parametro = 'name';
-        const name = 'Pedro'
-
+        const name = 'Pedro';
+        cy.request(`${endpoint}?${parametro}=${name}`).then(response =>{
+            const data = JSON.stringify(response.body)
+            const age = JSON.stringify(response.body.age)
+            const count = JSON.stringify(response.body.count)
+            const name = JSON.stringify(response.body.name)
+            cy.log(data)
+            cy.wait(20000)
+        })
     });
 });
